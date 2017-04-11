997e574ac95f308d8365ddf172371b2395a7f2e0
Unknown <smanc007@fiu.edu>
2017-04-11 18:13:46 -0400

    Creation of class code and fine tunning form design

--- STARSOrg/STARSOrg/CMember.vb
+++ STARSOrg/STARSOrg/CMember.vb
@@ -1,3 +1,105 @@
-﻿Public Class CMember
+﻿Imports System.Data.SqlClient
+Public Class CMember
+    'each line in the Member table
+    Private _mstrPantherID As String
+    Private _mstrFirstName As String
+    Private _mstrLastName As String
+    Private _mstrMiddleIn As String
+    Private _mstrEmail As String
+
+    'does this work since its a masked textbox
+    'Private _mstrPhoneNumber As String
+
+    'do i pass the string location of the picture
+    'Private _mstrPicture As String
+
+    Private _IsNewMember As Boolean
+
+    'Constructor for a New Member
+    Public Sub New()
+        _mstrFirstName = ""
+        _mstrLastName = ""
+        _mstrEmail = ""
+        _mstrMiddleIn = ""
+        _mstrPantherID = ""
+        '_mstrPhoneNumber=""
+        '_mstrPicture=""
+    End Sub
+
+
+#Region "Member Properties"
+
+    Public Property FirstName As String
+        Get
+            Return _mstrFirstName
+        End Get
+        Set(strVal As String)
+            _mstrFirstName = strVal
+        End Set
+    End Property
+
+    Public Property LastName As String
+        Get
+            Return _mstrLastName
+        End Get
+        Set(strval As String)
+            _mstrLastName = strval
+        End Set
+    End Property
+
+    Public Property Email As String
+        Get
+            Return _mstrEmail
+        End Get
+        Set(strval As String)
+            _mstrEmail = strval
+        End Set
+    End Property
+
+    Public Property MiddleIn As String
+        Get
+            Return _mstrMiddleIn
+        End Get
+        Set(strval As String)
+            _mstrMiddleIn = strval
+        End Set
+    End Property
+
+    Public Property PantherID As String
+        Get
+            Return _mstrPantherID
+        End Get
+        Set(strval As String)
+            _mstrPantherID = strval
+        End Set
+    End Property
+
+    'Public Property PhoneNumber As String
+    '    Get
+    '        Return _mstrPhoneNumber
+    '    End Get
+    '    Set(strval As String)
+    '        _mstrPhoneNumber = strval
+    '    End Set
+    'End Property
+
+    'Public Property Picture As String
+    '    Get
+    '        Return _mstrPicture
+    '    End Get
+    '    Set(strval As String)
+    '        _mstrPicture = strval
+    '    End Set
+    'End Property
+
+    Public Property IsNewMember As Boolean
+        Get
+            Return _IsNewMember
+        End Get
+        Set(blnval As Boolean)
+            _IsNewMember = blnval
+        End Set
+    End Property
+#End Region
 
 End Class
--- STARSOrg/STARSOrg/frmMembers.vb
+++ STARSOrg/STARSOrg/frmMembers.vb
@@ -1,5 +1,76 @@
-﻿Public Class frmMembers
-    'Private Sub picMemberPic_Click(sender As Object, e As EventArgs) Handles picMemberPic.Click
+﻿Imports System.Data.SqlClient
+Public Class frmMembers
+    Private objMembers As CMembers
 
-    'End Sub
+#Region "Toolbar"
+    Private Sub tsbMember_Click(sender As Object, e As EventArgs) Handles tsbMember.Click
+        'intNextAction = ACTION_MEMBER
+        'Me.Hide()
+
+        'nothing here already in the members tab
+
+    End Sub
+
+    Private Sub tsbRole_Click(sender As Object, e As EventArgs) Handles tsbRole.Click
+        intNextAction = ACTION_ROLE
+    End Sub
+
+    Private Sub tsbHome_Click(sender As Object, e As EventArgs) Handles tsbHome.Click
+        intNextAction = ACTION_HOME
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbCourse_Click(sender As Object, e As EventArgs) Handles tsbCourse.Click
+        intNextAction = ACTION_COURSE
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbEvent_Click(sender As Object, e As EventArgs) Handles tsbEvent.Click
+        intNextAction = ACTION_EVENT
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbHelp_Click(sender As Object, e As EventArgs) Handles tsbHelp.Click
+        intNextAction = ACTION_HELP
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbLogOut_Click(sender As Object, e As EventArgs) Handles tsbLogOut.Click
+        intNextAction = ACTION_LOGOUT
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbRSVP_Click(sender As Object, e As EventArgs) Handles tsbRSVP.Click
+        intNextAction = ACTION_RSVP
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbSemester_Click(sender As Object, e As EventArgs) Handles tsbSemester.Click
+        intNextAction = ACTION_SEMESTER
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbTutor_Click(sender As Object, e As EventArgs) Handles tsbTutor.Click
+        intNextAction = ACTION_TUTOR
+        Me.Hide()
+    End Sub
+    Private Sub tsbProxy_MouseEnter(sender As Object, e As EventArgs) Handles tsbHome.MouseEnter, tsbCourse.MouseEnter, tsbEvent.MouseEnter, tsbHelp.MouseEnter, tsbLogOut.MouseEnter, tsbMember.MouseEnter, tsbRole.MouseEnter, tsbRSVP.MouseEnter, tsbSemester.MouseEnter, tsbTutor.MouseEnter
+        'We need to do this only because we put our images in the BackgroundImage property instead of the Image property
+        Dim tsbProxy As ToolStripButton
+        tsbProxy = DirectCast(sender, ToolStripButton)
+        tsbProxy.DisplayStyle = ToolStripItemDisplayStyle.Text
+    End Sub
+    Private Sub tsbProxy_MouseLeave(sender As Object, e As EventArgs) Handles tsbHome.MouseLeave, tsbCourse.MouseLeave, tsbEvent.MouseLeave, tsbHelp.MouseLeave, tsbLogOut.MouseLeave, tsbMember.MouseLeave, tsbRole.MouseLeave, tsbRSVP.MouseLeave, tsbSemester.MouseLeave, tsbTutor.MouseLeave
+        'We need to do this only because we put our images in the BackgroundImage property instead of the Image property
+        Dim tsbProxy As ToolStripButton
+        tsbProxy = DirectCast(sender, ToolStripButton)
+        tsbProxy.DisplayStyle = ToolStripItemDisplayStyle.Image
+    End Sub
+
+
+#End Region
+    Private Sub frmMembers_Load(sender As Object, e As EventArgs) Handles Me.Load
+        objMembers = New CMembers
+    End Sub
+
 End Class
