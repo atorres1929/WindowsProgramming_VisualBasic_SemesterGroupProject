8cb9b3f5978c2641ae75a8321b59f4a9d3a2b0bd
Unknown <cjimi001@fiu.edu>
2017-04-11 18:16:32 -0400

    Events Changes

---
+++ STARSOrg/STARSOrg/CEvents.vb
@@ -0,0 +1,3 @@
+﻿Public Class CEvents
+
+End Class
--- STARSOrg/STARSOrg/frmEvents.vb
+++ STARSOrg/STARSOrg/frmEvents.vb
@@ -1,3 +1,65 @@
 ﻿Public Class frmEvents
+#Region "Toolbar"
+    Private Sub tsbMember_Click(sender As Object, e As EventArgs) Handles tsbMember.Click
+        intNextAction = ACTION_MEMBER
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbRole_Click(sender As Object, e As EventArgs) Handles tsbRole.Click
+        'nothing to do here - already on the Role screen
+    End Sub
+
+    Private Sub tsbHome_Click(sender As Object, e As EventArgs) Handles tsbHome.Click
+        intNextAction = ACTION_HOME
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbCourse_Click(sender As Object, e As EventArgs) Handles tsbCourse.Click
+        intNextAction = ACTION_COURSE
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbEvent_Click(sender As Object, e As EventArgs) Handles tsbEvent.Click
+        intNextAction = ACTION_EVENT
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbHelp_Click(sender As Object, e As EventArgs) Handles tsbHelp.Click
+        intNextAction = ACTION_HELP
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbLogOut_Click(sender As Object, e As EventArgs) Handles tsbLogOut.Click
+        intNextAction = ACTION_LOGOUT
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbRSVP_Click(sender As Object, e As EventArgs) Handles tsbRSVP.Click
+        intNextAction = ACTION_RSVP
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbSemester_Click(sender As Object, e As EventArgs) Handles tsbSemester.Click
+        intNextAction = ACTION_SEMESTER
+        Me.Hide()
+    End Sub
+
+    Private Sub tsbTutor_Click(sender As Object, e As EventArgs) Handles tsbTutor.Click
+        intNextAction = ACTION_TUTOR
+        Me.Hide()
+    End Sub
+    Private Sub tsbProxy_MouseEnter(sender As Object, e As EventArgs) Handles tsbHome.MouseEnter, tsbCourse.MouseEnter, tsbEvent.MouseEnter, tsbHelp.MouseEnter, tsbLogOut.MouseEnter, tsbMember.MouseEnter, tsbRole.MouseEnter, tsbRSVP.MouseEnter, tsbSemester.MouseEnter, tsbTutor.MouseEnter
+        'We need to do this only because we put our images in the BackgroundImage property instead of the Image property
+        Dim tsbProxy As ToolStripButton
+        tsbProxy = DirectCast(sender, ToolStripButton)
+        tsbProxy.DisplayStyle = ToolStripItemDisplayStyle.Text
+    End Sub
+    Private Sub tsbProxy_MouseLeave(sender As Object, e As EventArgs) Handles tsbHome.MouseLeave, tsbCourse.MouseLeave, tsbEvent.MouseLeave, tsbHelp.MouseLeave, tsbLogOut.MouseLeave, tsbMember.MouseLeave, tsbRole.MouseLeave, tsbRSVP.MouseLeave, tsbSemester.MouseLeave, tsbTutor.MouseLeave
+        'We need to do this only because we put our images in the BackgroundImage property instead of the Image property
+        Dim tsbProxy As ToolStripButton
+        tsbProxy = DirectCast(sender, ToolStripButton)
+        tsbProxy.DisplayStyle = ToolStripItemDisplayStyle.Image
+    End Sub
+#End Region
 
 End Class
