f964946e8d02b48a488488d5993d9c284adafbdb
Adam Torres <atorres1929@gmail.com>
2017-04-18 00:14:37 -0400

    Adam Added script for you guys to run so that you can easily add guest and bad login into your databases

---
+++ SecuritySP/Insert Guest and Bad Login.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO MEMBER (PID, FNAME, LNAME, MI, EMAIL, PHONE) VALUES(
+	0000001,
+	'Guest',
+	'Guest',
+	'G',
+	'guest@fiu.edu',
+	'3055555555'
+)
+INSERT INTO MEMBER (PID, FNAME, LNAME, MI, EMAIL, PHONE) VALUES(
+	9999999,
+	'Unsuccessful',
+	'Bad',
+	'E',
+	'noreply@fiu.edu',
+	'3055555555'
+)
+INSERT INTO SECURITY (PID, UserID, Password, SecRole) VALUES(
+	0000001,
+	'Guest',
+	'Guest',
+	'Guest'
+)
